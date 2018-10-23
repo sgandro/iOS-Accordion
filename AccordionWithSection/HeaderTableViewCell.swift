5bff3707b622d315da041f4690d4631ab18a454a
Alessandro Perna <almavivadeveloper@MacBook-Pro-di-AlePerna.local>
2018-10-23 16:42:17 +0200

    first commit

---
+++ AccordionWithSection/HeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTableViewCell.swift
+//  AccordionWithSection
+//
+//  Created by Alessandro Perna on 16/10/2018.
+//  Copyright © 2018 Alessandro Perna. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
